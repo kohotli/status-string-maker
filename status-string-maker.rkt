18141ee952f0d536d83d8417c28bb137c4709d96
Amelia Carter <ameliamarycarter@gmail.com>
2019-03-31 12:49:59 -0700

    Now shows when MPD is disconnected.

--- status-string-maker.rkt
+++ status-string-maker.rkt
@@ -18,18 +18,19 @@
 ;Command output parsing
 (define (get-command-output-string command)
   (let ([command-path (find-executable-path command)])
-    (if (not command-path) (error "Error (get-command-output-string): Command not found")
-    (strip-newline (with-output-to-string (λ () (system* command-path)))))))
+    ;(if (not command-path) (error "Error (get-command-output-string): Command not found")
+    (if (not command-path) "MPD Disconnected"
+        (strip-newline (with-output-to-string (λ () (system* command-path)))))))
 
 ;Command output parsing, accepting arguments in a list
 (define (get-command-output-with-args command args)
- (let ([command-path (find-executable-path command)])
-  (if (not command-path) (error "Error (get-command-output-with-args-string): Command not found")
-   (string-split (strip-newline (with-output-to-string
-                                 (λ () (apply system* (cons command-path args)))))))))
+  (let ([command-path (find-executable-path command)])
+    (if (not command-path) (error "Error (get-command-output-with-args-string): Command not found")
+        (string-split (strip-newline (with-output-to-string
+                                       (λ () (apply system* (cons command-path args)))))))))
 
 (define (get-command-output-list command)
- (string-split (get-command-output-string command)))
+  (string-split (get-command-output-string command)))
 
 ;User@Hostname functions
 (define (get-current-user) (get-command-output-string "whoami"))
@@ -39,15 +40,15 @@
 
 ;Network Functions
 (define (get-ip-out-list device)
- (let ([command-path (find-executable-path "ip")])
-  (if (not command-path) (error "Error in (get-ip-out device): ip command not found")
-   (string-split (with-output-to-string (λ () (system* command-path "addr" "show" device)))))))
+  (let ([command-path (find-executable-path "ip")])
+    (if (not command-path) (error "Error in (get-ip-out device): ip command not found")
+        (string-split (with-output-to-string (λ () (system* command-path "addr" "show" device)))))))
 
 (define (get-ip-addr device)
- (let* ([unparsed-ip-out (get-ip-out-list device)]
-        [filtered-ip-out (member "inet" unparsed-ip-out)])
-  (if (not filtered-ip-out) "Disconnected"
-   (string-trim (second filtered-ip-out) "/24"))))
+  (let* ([unparsed-ip-out (get-ip-out-list device)]
+         [filtered-ip-out (member "inet" unparsed-ip-out)])
+    (if (not filtered-ip-out) "Disconnected"
+        (string-trim (second filtered-ip-out) "/24"))))
 
 ;String handling
 (define (strip-commas str) (string-trim str ","))
@@ -58,64 +59,67 @@
 (define (get-battery-status acpi-out) (strip-commas (third acpi-out)))
 
 (define (compose-battery-state charging-icon)
- (let* ([acpi (get-acpi)]
-        [battery-level (get-battery-level acpi)]
-        [battery-status (get-battery-status acpi)]
-        [battery-charging (if (equal? "Charging" battery-status) charging-icon "")])
-  (string-append battery-charging battery-level)))
+  (let* ([acpi (get-acpi)]
+         [battery-level (get-battery-level acpi)]
+         [battery-status (get-battery-status acpi)]
+         [battery-charging (if (equal? "Charging" battery-status) charging-icon "")])
+    (string-append battery-charging battery-level)))
 
 ;Get current time
 (define (get-time date-format)
- (let ([command-path (find-executable-path "date")])
-  (if (not command-path) (error "Error in (get-time): date command not found")
-   (strip-newline (with-output-to-string (λ () (system* command-path date-format)))))))
+  (let ([command-path (find-executable-path "date")])
+    (if (not command-path) (error "Error in (get-time): date command not found")
+        (strip-newline (with-output-to-string (λ () (system* command-path date-format)))))))
 (define (get-current-date) (get-time "+%F"))
 
 ;MPD Music functions
+
 (define (get-mpd-now-playing artist-song-separator host port)
- (let* ([mpc-output (get-command-output-with-args "mpc" (list "-h" host "-p" port))]
-        [title-index
-        (if (index-of mpc-output "[playing]")
-         (index-of mpc-output "[playing]")
-         (index-of mpc-output "[paused]"))]
-        [playstring-list (take mpc-output title-index)])
-  (string-join (list-set playstring-list (index-of mpc-output "-") artist-song-separator))))
+  (let ([mpc-output (get-command-output-with-args "mpc" (list "-h" host "-p" port))])
+    (if (string-contains? (string-join mpc-output) "error")
+        (let* ([title-index
+                 (if (index-of mpc-output "[playing]")
+                     (index-of mpc-output "[playing]")
+                     (index-of mpc-output "[paused]"))]
+               [playstring-list (take mpc-output title-index)])
+          (string-join playstring-list))
+        "MPD Disconnected")))
 
 ;Call xsetroot
 (define (xsetroot status-string)
- (let ([command-path (find-executable-path "xsetroot")])
-  (if (not command-path) (error "Error in (xsetroot): xsetroot command not found")
-   (system* command-path "-name" status-string))))
+  (let ([command-path (find-executable-path "xsetroot")])
+    (if (not command-path) (error "Error in (xsetroot): xsetroot command not found")
+        (system* command-path "-name" status-string))))
 
 ;Configure icons, separator, date format, and internet devices here.
 (define (main sleep-length)
- (xsetroot (statusline-render
-            "enp0s31f6"   ;Internet device
-            "+%R"         ;Date format
-            "\uE1A3 "     ;Battery charging icon
-            "\uE1BA "     ;Icon for IP addr display
-            "\uE405 "     ;Icon for mpd music display
-            " | "         ;Separator. The spaces are highly recommended to keep.
-            ;"192.168.1.4" ;MPD host IP. Defaults to 127.0.0.1
-            ;"6600"))      ;MPD host port. Defaults to 6600
- (sleep sleep-length)
- (main sleep-length))
+  (xsetroot (statusline-render
+              "enp0s31f6"   ;Internet device
+              "+%R"         ;Date format
+              "\uE1A3 "     ;Battery charging icon
+              "\uE1BA "     ;Icon for IP addr display
+              "\uE405 "     ;Icon for mpd music display
+              " | "         ;Separator. The spaces are highly recommended to keep.
+              "192.168.1.4" ;MPD host IP. Defaults to 127.0.0.1
+              "6600"))      ;MPD host port. Defaults to 6600
+  (sleep sleep-length)
+  (main sleep-length))
 
 ;Statusline render. Edit this to configure the output for your setup. To remove something just comment that line out.
 (define (statusline-render
-         inet-device
-         date-format
-         charging-icon
-         inet-icon
-         music-icon
-         separator
-         [mpd-host "127.0.0.1"]
-         [mpd-port "6600"])
- (string-append
-  music-icon (get-mpd-now-playing "»" mpd-host mpd-port) separator
-  (compose-battery-state charging-icon) separator
-  inet-icon (get-ip-addr inet-device) separator
-  "[" (user-at-host-string) "]" separator
-  (get-current-date) " " (get-time date-format)))
+          inet-device
+          date-format
+          charging-icon
+          inet-icon
+          music-icon
+          separator
+          [mpd-host "127.0.0.1"]
+          [mpd-port "6600"])
+  (string-append
+    music-icon (get-mpd-now-playing "»" mpd-host mpd-port) separator
+    ;(compose-battery-state charging-icon) separator
+    inet-icon (get-ip-addr inet-device) separator
+    "[" (user-at-host-string) "]" separator
+    (get-current-date) " " (get-time date-format)))
 
-(main 5)
+(main 1)
